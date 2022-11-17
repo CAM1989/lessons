59c2c45d88f8805a01e54be5245cebef7d22cc8c
CAM1989 <77327212+CAM1989@users.noreply.github.com>
2022-11-17 15:22:07 +0300

    Revert "hw3_2"

--- src/main/java/ru/gb/Lesson3/Counter.java
+++ src/main/java/ru/gb/Lesson3/Counter.java
@@ -4,13 +4,13 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Counter {
-    private static final int ITERATIONS = 100;
+    private static final int iterations = 100;
     private static int value = 0;
     private static Lock lock = new ReentrantLock();
 
     public static void main(String[] args) throws InterruptedException {
         Thread thread1 = new Thread(() -> {
-            for (int i = 0; i < ITERATIONS / 2; i++) {
+            for (int i = 0; i < iterations / 2; i++) {
                 lock.lock();
                 value++;
                 System.out.println("Count 1 - " + value);
@@ -19,7 +19,7 @@ public class Counter {
         });
 
         Thread thread2 = new Thread(() -> {
-            for (int i = 0; i < ITERATIONS / 2; i++) {
+            for (int i = 0; i < iterations / 2; i++) {
                 lock.lock();
                 value++;
                 System.out.println("Count 2 - " + value);
